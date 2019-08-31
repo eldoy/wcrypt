0b86a17273ad8831c9361759af262ccb215ff163
Vidar Eldøy <vidar@eldoy.com>
2019-08-31 23:44:36 +0200

    Add some more tests

--- README.md
+++ README.md
@@ -7,12 +7,20 @@ Encrypt and decrypt strings with Node.js
 
 ### Usage
 ```javascript
-const wcrypt = require('wcrypt')()
+/* Default options shown */
+const wcrypt = require('wcrypt')({
+  algorithm: 'aes-192-cbc',
+  password: 'password',
+  salt: 'salt',
+  length: 24
+})
 
+/* Encrypt a string */
 const encrypted = await wcrypt('clear text string')
 // e1ec7507554a2ce8c5d13b01f803c64f3b95c08d8a9b61e964f78d7656bbecb9
 
+/* Decrypt a string */
 const decrypted = await wcrypt(encrypted)
-// 'clear text string'
+// clear text string
 ```
 MIT licensed. Enjoy!
--- test/crypto.spec.js
+++ test/crypto.spec.js
@@ -1,12 +1,45 @@
-const wcrypt = require('../index.js')()
+const wcrypt = require('../index.js')
 
 describe('crypto', () => {
   it('should encrypt and decrypt a string', async () => {
-    const encrypted = await wcrypt.encrypt('string')
+    const crypt = wcrypt()
+    const encrypted = await crypt.encrypt('string')
     expect(encrypted).toBeDefined()
     expect(encrypted.length).toBe(32)
 
-    const decrypted = await wcrypt.decrypt(encrypted)
+    const decrypted = await crypt.decrypt(encrypted)
     expect(decrypted).toBe('string')
+  })
+
+  it('should not encrypt and decrypt a string using wrong password', async () => {
+    const crypt1 = wcrypt({ password: 'hello' })
+    const encrypted = await crypt1.encrypt('string')
+    expect(encrypted).toBeDefined()
+    expect(encrypted.length).toBe(32)
+
+    const crypt2 = wcrypt({ password: 'bye' })
+    let fail
+    try {
+      await crypt2.decrypt(encrypted)
+    } catch (e) {
+      fail = e.message
+    }
+    expect(fail).toBe('error:06065064:digital envelope routines:EVP_DecryptFinal_ex:bad decrypt')
+  })
+
+  it('should not encrypt and decrypt a string using wrong salt', async () => {
+    const crypt1 = wcrypt({ salt: 'hello' })
+    const encrypted = await crypt1.encrypt('string')
+    expect(encrypted).toBeDefined()
+    expect(encrypted.length).toBe(32)
+
+    const crypt2 = wcrypt({ salt: 'bye' })
+    let fail
+    try {
+      await crypt2.decrypt(encrypted)
+    } catch (e) {
+      fail = e.message
+    }
+    expect(fail).toBe('error:06065064:digital envelope routines:EVP_DecryptFinal_ex:bad decrypt')
   })
 })
